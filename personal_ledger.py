44a7a4c8bac42c588b0b14ad7f8fca1b06e346cc
George Schneeloch <bostonbusmap@gmail.com>
2013-07-07 23:16:14 -0400

    Flesh out accounts

--- personal_ledger.py
+++ personal_ledger.py
@@ -27,11 +27,21 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 @app.route('/')
-def show_entries():
-    cur = g.db.execute('select title, id from accounts')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('accounts.html', entries=entries)
+def index():
+    return redirect(url_for('accounts'))
 
+@app.route('/accounts')
+def accounts():
+    cur = g.db.execute('SELECT accounts.full_title, SUM(withdrawal), SUM(deposit) FROM accounts LEFT OUTER JOIN transactions ON transactions.account_id = accounts.id GROUP BY accounts.id ORDER BY accounts.full_title')
+    accounts = [dict(title=row[0], withdrawal=row[1] or 0.0, deposit=row[2] or 0.0) for row in cur.fetchall()]
+    return render_template('accounts.html', accounts=accounts)
+
+@app.route('/add_account', methods=['POST', 'GET'])
+def add_account():
+    if request.method == 'POST':
+        flash("TODO: submit account info")
+        return redirect(url_for('accounts'))
+    return render_template('add_account.html')
 
 
 if __name__ == "__main__":
